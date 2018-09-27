39a24a14133d284c37cd6874f3a82866cf5e4add
ito <yoshiaki110@gmail.com>
2018-09-27 15:05:46 +0900

    bug fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const noble = require('noble');
 
 const NAME = 'IM';
 const ADDRESS = 'e7e6a420bdcf';
-const INTERVAL_MILLISEC = 1000;
+const INTERVAL_MILLISEC = 100;
 
 //discovered BLE device
 const discovered = (peripheral) => {
@@ -13,17 +13,23 @@ const discovered = (peripheral) => {
     uuid: peripheral.uuid,
     rssi: peripheral.rssi
   };
-  if(NAME === device.name && ADDRESS === device.uuid) {
+  if (NAME === device.name && ADDRESS === device.uuid) {
     const v = peripheral.advertisement.manufacturerData.toString('hex').substring(30, 34);
     var s = v.substring(2,4) + v.substring(0,2);
-    var d = parseInt(s, 16) * 360 / 65535 - 180;
+    var d = parseInt(s, 16) * 360 / 65535;
+    //  0     90     180
+    // 55      0
+    //       360     305
     console.log(d);
-    if (d > 0) {
-      d = (d - 180) * 180 / (-55);
-    } else {
-      d = (d + 125) * 90 / (-55);
+    if (d < 55) {
+      d = (d * -1 + 55) * 90 / 55;
+      console.log('+   ' + parseInt(d));
+    } else if (d < 305) {
+      ; // nop
+    } else if (d <= 360) {
+      d = (360 - d) * 90 / 55 + 90;
+      console.log('-   ' + parseInt(d));
     }
-    console.log(d);
     console.log('');
   }
 }
@@ -34,8 +40,8 @@ const scanStart = () => {
   noble.on('discover', discovered);
 }
 
-if(noble.state === 'poweredOn'){
+if (noble.state === 'poweredOn'){
   scanStart();
-}else{
+} else {
   noble.on('stateChange', scanStart);
 }
